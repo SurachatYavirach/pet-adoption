a8123421164801acfce89fe8465fe618bb08a34f
Hong <yavirach.s@gmail.com>
2024-05-03 03:07:54 +0700

    Update description, age, img with js

--- main.js
+++ main.js
@@ -16,7 +16,13 @@ async function petsArea() {
   const petsData = await petsPromise.json()
   petsData.forEach(pet => {
     const clone = template.content.cloneNode(true)
+
     clone.querySelector("h3").textContent = pet.name
+    clone.querySelector(".pet-description").textContent = pet.description
+    clone.querySelector(".pet-age").textContent = createAgeText(pet.birthYear)
+    clone.querySelector(".pet-card-photo img").src = pet.photo
+    clone.querySelector(".pet-card-photo img").alt = `A ${pet.species} named ${pet.name}`
+
     wrapper.appendChild(clone)
   })
   document.querySelector(".list-of-pets").appendChild(wrapper)
@@ -24,3 +30,11 @@ async function petsArea() {
 
 petsArea()
 
+function createAgeText(birthYear) {
+  const currentYear = new Date().getFullYear()
+  const petAge = currentYear - birthYear
+
+  if (petAge == 1) return "1 year old"
+  if (petAge == 0) return "Less than year old"
+  return `${petAge} years old`
+}
